689cacea99a71a81e03be829638e6269de277c25
antonyboivin <antonyboivin@outlook.fr>
2017-07-04 22:20:17 +0200

    Le labyrinthe s'affiche. modification de body_game.py et brouillon.py, ajout images dans /sprites : finish.png, start.png, floor.png, wall.png

--- body_game.py
+++ body_game.py
@@ -4,23 +4,24 @@
 import os                   #Allows to interact with the operating system
 from tkinter import *
 
-class Labyrinth(object):
-    "Class to define a window of the application"
-    def __init__(self,master):
+class Labyrinth:
+    "Class to define the window of the application"
+    def __init__(self, window):
         "Constructor of the main window"
-        self.root = master
-        self.root.title('Help MacGyver escape !')
-        self.labyrinth = Canvas(self.root, width = 600, height = 600, bg = 'gray' )
-        self.labyrinth.pack(side = TOP, padx = 5, pady = 5)
-        Labyrinth.labyrinth_open('map_one.txt')
+        self.window = window 
+        self.window.title('Help MacGyver escape !')
+        data_labyrinth = self.labyrinth_open('map_one.txt')
+        self.labyrinth_display(data_labyrinth, self.window)
 
-        
 
-    def labyrinth_open(levelFile):
+
+    def labyrinth_open(self, levelFile):
+        print("labyrinth_open")
         """Function that tries to open the name.txt of the level
         Returns a custom error message in case of exeption"""
         directory = os.path.dirname(__file__)
-        path_to_file = os.path.join(directory,"sprites", levelFile)
+        path_to_file = os.path.join(directory, "sprites", levelFile)
+
         try:
             with open(path_to_file, "r") as f:
                 data = f.readlines()
@@ -31,19 +32,54 @@ class Labyrinth(object):
         except Exception as e:
             raise e
 
-        for i in range(len(data)) :
-            data[i] = data[i].strip()
-        print(data)
+        return data
 
-    def labyrinth_display(level):
+
+    def labyrinth_display(self, dataLevel, window):
+        print("labyrinth_display")
         "Load sprites"
-        self.wall=PhotoImage(file = "sprites/wall.gif")
+        self.wall=PhotoImage(file = "sprites/wall.png")
+        self.floor=PhotoImage(file = "sprites/floor.png")
+        self.finish=PhotoImage(file = "sprites/finish.png")
+        self.start=PhotoImage(file = "sprites/start.png")
 
-    
-    
+        self.size_sprite = 50
+
+        self.labyrinth = Canvas(window, width = 750, height = 750)
+        self.labyrinth.pack(side = TOP, padx = 5, pady = 5)
+        #self.labyrinth.create_image(0, 0, anchor=NW, image= self.wall)
+
+
+        for i in range(len(dataLevel)) :
+            dataLevel[i] = dataLevel[i].strip()
+            print(dataLevel[i])
+
+        n_ligne = 0
+        for ligne in dataLevel:
+            n_col = 0
+            for car in ligne :
+                
+                # Murs
+                if car == "w":
+                    self.labyrinth.create_image(n_col, n_ligne, anchor=NW, image= self.wall)
+                #sols
+                if car == "0":
+                    self.labyrinth.create_image(n_col, n_ligne, anchor=NW, image= self.floor)
+                #start
+                if car == "s":
+                    self.labyrinth.create_image(n_col, n_ligne, anchor=NW, image= self.start)
+                #Finish
+                if car == "f":
+                    self.labyrinth.create_image(n_col, n_ligne, anchor=NW, image= self.finish)
+
+            
+                n_col += self.size_sprite
+            n_ligne += self.size_sprite
+
+
 
 if __name__ == "__main__":
     
-    root = Tk()
-    labyrinth = Labyrinth(root)
-    root.mainloop()
+    window = Tk()
+    labyrinth = Labyrinth(window)
+    window.mainloop()
--- brouillon.py
+++ brouillon.py
@@ -1,31 +1,84 @@
 #! /usr/bin/env python 3
 # coding : utf-8
 
-import os					#Allows to interact with the operating system
+import os                   #Allows to interact with the operating system
 from tkinter import *
 
-class Labyrinth(object):
+class Labyrinth:
     "Class to define a window of the application"
-    def __init__(self,master):
+    def __init__(self, window):
         "Constructor of the main window"
-        self.root = master
-        self.root.title('Help MacGyver escape !')
-        self.laby = Canvas(self.root, width = 500, height = 500, bg = 'gray' )
-        self.laby.pack(side = TOP, padx = 5, pady = 5)
-    
-        data=[]
-        f = open("sprites/map_one.txt")
-        data = f.readlines()
-        f.close()
-        for i in range(len(data)):
-            data[i] = data[i].strip()
+        self.window = window 
+        self.window.title('Help MacGyver escape !')
+        data_labyrinth = self.labyrinth_open('map_one.txt')
+        self.labyrinth_display(data_labyrinth, self.window)
 
-        print(data)
-        
 
-    
+
+    def labyrinth_open(self, levelFile):
+        print("labyrinth_open")
+        """Function that tries to open the name.txt of the level
+        Returns a custom error message in case of exeption"""
+        directory = os.path.dirname(__file__)
+        path_to_file = os.path.join(directory, "sprites", levelFile)
+
+        try:
+            with open(path_to_file, "r") as f:
+                data = f.readlines()
+                 
+        except FileNotFoundError as e:
+            print("The file was not found !")
+            print("Please check that the folder 'sprites' contains 'map_one.txt'")
+        except Exception as e:
+            raise e
+
+        return data
+
+
+    def labyrinth_display(self, dataLevel, window):
+        print("labyrinth_display")
+        "Load sprites"
+        self.wall=PhotoImage(file = "sprites/wall.gif")
+
+        self.largeur_block = 50
+        self.hauteur_block = 50                                     #un bloc fait 40 px
+        self.nb_blocs = 15                                          #succession de 15 blocs cote à cote
+        self.centre = self.largeur_block /2                         #le centre se trouve au milieu d'un bloc
+        self.ligne = self.nb_blocs * self.largeur_block
+        self.colonne = self.nb_blocs * self.hauteur_block
+        self.size_sprite = 50
+
+        self.labyrinth = Canvas(window, width = 750, height = 750)
+        self.labyrinth.pack(side = TOP, padx = 5, pady = 5)
+        #self.labyrinth.create_image(0, 0, anchor=NW, image= self.wall)
+        #self.labyrinth.create_image(0, 0, image= self.wall)
+        #self.labyrinth.create_image.pack()
+
+        for i in range(len(dataLevel)) :
+            dataLevel[i] = dataLevel[i].strip()
+            print(dataLevel[i])
+
+        n_ligne = 0
+        for ligne in dataLevel:
+            n_col = 0
+            n_ligne += self.size_sprite
+            for car in ligne :
+                n_col += self.size_sprite
+                # Murs
+                if car == "w":
+                    self.labyrinth.create_image(n_col-50, n_ligne-50, anchor=NW, image= self.wall)
+
+                    
+                    
+                    
+
+
+        self.labyrinth.pack()
+
+
 
 if __name__ == "__main__":
-    root = Tk()
-    laby = Labyrinth(root)
-    root.mainloop()
+    
+    window = Tk()
+    labyrinth = Labyrinth(window)
+    window.mainloop()
